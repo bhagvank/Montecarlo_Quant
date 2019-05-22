f8d01735a5331ebec0f694e004795a78ce175a47
gkommadi <gkommadi@gmail.com>
2019-05-22 08:18:37 -0700

    Node Changes

---
+++ classical/node/index.js
@@ -0,0 +1,7 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World\n');
+}).listen(8083);
+console.log('Server running at http://127.0.0.1:8083/');
