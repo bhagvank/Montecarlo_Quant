881cb4804227d14eec97265c50affb1784477584
Ojasjain2017@gmail.com <ojasjain2017@gmail.com>
2019-06-05 22:25:43 +0530

    added webapp

--- classical/webapp/montecarlo/views.py
+++ classical/webapp/montecarlo/views.py
@@ -12,6 +12,7 @@ from math import exp, sqrt, log
 from random import gauss, seed
 import scipy as sp
 import numpy as np
+import datetime
 
 import os
 import logging
@@ -242,3 +243,52 @@ def asian_montecarlo(request):
     context = {'option_value': call_price,'time_taken': time_taken}
 
     return render(request, template_name,context)
+
+def generate_asset_price(S,v,r,T):
+    return S * exp((r - 0.5 * v**2) * T + v * sqrt(T) * gauss(0,1.0))
+
+def call_payoff(S_T,K):
+    return max(0.0,S_T-K)
+
+# S = 857.29 # underlying price
+# v = 0.2076 # vol of 20.76%
+# r = 0.0014 # rate of 0.14%
+
+# K = 860.
+# simulations = 90000
+
+def black_sholes_option(request):
+
+
+    template_name = 'montecarlo/black_sholes_option.html'
+
+    return render(request, template_name)
+
+def black_sholes_montecarlo(request):
+    initial_time = time()
+    template_name='montecarlo/black_sholes_montecarlo.html'
+    
+    S = float(request.POST["underlying_price"])
+    v = float(request.POST["volume"])
+    T = (datetime.date(2013,9,21) - datetime.date(2013,9,3)).days / 365.0
+    r = float(request.POST["rate"])
+
+#    r = float(request.POST["risk"])
+
+    K = float(request.POST["K"])
+
+    simulations= int(request.POST["simulations"])
+
+    payoffs = []
+    discount_factor = math.exp(-r * T)
+
+    for i in xrange(simulations):
+        S_T = generate_asset_price(S,v,r,T)
+        payoffs.append(
+            call_payoff(S_T, K)
+        )
+
+    price = discount_factor * (sum(payoffs) / float(simulations))
+    time_taken = time() - initial_time
+    context = {'price': price,'time_taken': time_taken}
+    return render(request,template_name,context)
