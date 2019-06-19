afc6240cd34d1b7673952f8411336ca069170d1a
Ojasjain2017@gmail.com <ojasjain2017@gmail.com>
2019-06-19 19:31:00 +0530

    new webapp

--- classical/webapp/montecarlo/LeastSquareMontecarlo.py
+++ classical/webapp/montecarlo/LeastSquareMontecarlo.py
@@ -5,22 +5,6 @@ warnings.filterwarnings("ignore")
 from sys import version
 
 class LeastSquareMontecarlo(object):
-    """ Class for American options pricing using Longstaff-Schwartz (2001):
-    "Valuing American Options by Simulation: A Simple Least-Squares Approach."
-    Review of Financial Studies, Vol. 14, 113-147.
-    S0 : float : initial stock/index level
-    strike : float : strike price
-    T : float : time to maturity (in year fractions)
-    M : int : grid or granularity for time (in number of total points)
-    r : float : constant risk-free short rate
-    div :    float : dividend yield
-    sigma :  float : volatility factor in diffusion term
-
-    Unitest(doctest):
-    >>> AmericanPUT = LeastSquareMontecarlo('put', 36., 40., 1., 50, 0.06, 0.06, 0.2, 10000  )
-    >>> AmericanPUT.price
-    4.4731177017712209
-    """
 
     def __init__(self, option_type, S0, strike, T, M, r, div, sigma, simulations):
         try:
