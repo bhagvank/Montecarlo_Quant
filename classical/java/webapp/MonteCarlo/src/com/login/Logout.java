21cb7f8baedfaf388b0d9d88cdeeaa3ba792619e
madhav28 <madhav2800@gmail.com>
2019-11-09 12:43:40 +0530

    Added Java Maven Application

---
+++ classical/java/webapp/MonteCarlo/src/com/login/Logout.java
@@ -0,0 +1,28 @@
+package com.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		session.removeAttribute("username");
+		session.invalidate();
+		PrintWriter out =  response.getWriter();
+        out.println("Successfully logged out!! You can close the current page.");
+	}
+	
+}
